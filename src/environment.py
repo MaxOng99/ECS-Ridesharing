236773fe0be76a4d43b2e2e618c3d1e0458758ed
MaxOng99 <ongyicheng99@gmail.com>
2021-06-23 01:17:58 +0100

    Generate passengers and assign start and end locations to them

--- src/environment.py
+++ src/environment.py
@@ -1,28 +1,43 @@
+import random
 import numpy as np
-from igraph import Graph
 import igraph as ig
 
-class Environment:
-    def __init__(self,
-                 num_locations=10,
-                 max_coordinates=(100, 100),
-                 avg_vehicle_speed=2.0):
+class Passenger:
 
-        self.graph = self.generate_graph(num_locations, max_coordinates, avg_vehicle_speed)
-
-    def generate_graph(self, num_locations, max_coordinates, avg_speed):
+    def __init__(self, id, start, destination):
         """
         Args:
-            num_locations (Int): Total number of locations
-            max_coordinates ((Int, Int))): Largest coordinates - specifies the boundary of euclidean space
-            avg_speed [Float]: Speed in km/min
+            start (int): Node ID of start location
+            destination (int): Node ID of destination
+        """
+        self.id = id
+        self.start_id = start
+        self.destination_id = destination
+    
+    def __str__(self):
+        return f"Passenger {self.id}: {{Start: {self.start_id} | End: {self.destination_id}}}"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
 
-        Returns:
-            Graph: Fully connected graph, satisfying triangle inequality with travel time as edge weight
+class Environment:
+    def __init__(self, num_locations=10, num_passengers=10, max_coordinates=(100, 100), avg_vehicle_speed=2.0):
+        """
+        Args:
+            max_coordinates ((int, int)): Largest coordinates - specifies the boundary of euclidean space
+            avg_vehicle_speed (float): Speed in km/min
         """
 
+        self.num_locations = num_locations
+        self.num_passengers = num_passengers
+        self.max_coordinates = max_coordinates
+        self.avg_speed = avg_vehicle_speed
+        self.graph = self.generate_graph(num_locations, max_coordinates, avg_vehicle_speed)
+        self.passengers = self.generate_passengers(self.graph, num_passengers)
+
+    def generate_graph(self, num_locations, max_coordinates, avg_speed):
         # Create fully connected graph with n locations
-        g = Graph.Full(n=num_locations, loops=False)
+        g = ig.Graph.Full(n=num_locations, loops=False)
         
         # Generate random coordinates
         max_x = max_coordinates[0]
@@ -47,7 +62,20 @@ class Environment:
             g.es[edge_id]['time'] = round(distance / avg_speed, 2)
                     
         return g
-    
+
+    def generate_passengers(self, graph, num_passengers):
+
+        location_ids = [location_id for location_id in range(len(graph.vs['coordinate']))]
+        passengers = []
+
+        for id in range(num_passengers):
+
+            # Sample 2 elements w/o replacement - avoids having same start and end location
+            start_id, destination_id = random.sample(location_ids, 2)
+            passengers.append(Passenger(id, start_id, destination_id))
+
+        return passengers
+
     def plot_graph(self):
         visual_style = {
             "bbox": (600, 600),
@@ -56,7 +84,8 @@ class Environment:
         self.graph.vs['label'] = self.graph.vs['coordinate']
         self.graph.es['label'] = self.graph.es['time']
         ig.plot(self.graph, **visual_style)
+    
 
-np.random.seed(42)
 env = Environment(num_locations=5)
 env.plot_graph()
+
