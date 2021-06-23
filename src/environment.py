d3e06067e1e6275752a34cf9715e9d0f767f5a7e
MaxOng99 <ongyicheng99@gmail.com>
2021-06-23 01:24:31 +0100

    Add string representation for Environment object

--- src/environment.py
+++ src/environment.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import igraph as ig
+from prettytable import PrettyTable
 
 class Passenger:
 
@@ -84,8 +85,45 @@ class Environment:
         self.graph.vs['label'] = self.graph.vs['coordinate']
         self.graph.es['label'] = self.graph.es['time']
         ig.plot(self.graph, **visual_style)
-    
 
-env = Environment(num_locations=5)
-env.plot_graph()
+    def __str__(self):
+        
+        """
+        Returns:
+            string: Displays the summary of the environment
+        """
+        # Display Environment Properties
+        environment_properties = PrettyTable()
+        environment_properties.field_names = ["Properties", "Value"]
+        environment_properties.add_rows([["num_locations", self.num_locations],
+        ["num_passengers", self.num_passengers],
+        ["vehicle speed", f"{self.avg_speed} km/min"]])
+
+        # Display Passenger Details
+        passengers = [[p.id, p.start_id, p.destination_id] for p in self.passengers]
+        passenger_list = PrettyTable()
+        passenger_list.add_rows(passengers)
+        passenger_list.field_names = ["Passenger ID", "Start Location ID", "Destination ID"]
+
+        # Display Graph Details
+        graph_properties = PrettyTable()
+        graph_properties_row = []
+        for location_1, location_2 in self.graph.get_edgelist():
+
+            edge_id = self.graph.get_eid(location_1, location_2)
+            row_data = [edge_id, location_1, location_2, self.graph.es[edge_id]['time']]
+            graph_properties_row.append(row_data)
+        
+        graph_properties.add_rows(graph_properties_row)
+        graph_properties.field_names = ["Edge ID", "Location 1", "Location 2", "Time Taken (min)"]
+        
+        return "\n".join(["Environment Properties:",
+        f"{environment_properties.get_string()} \n",
+        "Passengers:",
+        f"{passenger_list.get_string()} \n",
+        "Graph:",
+        graph_properties.get_string()])
+
+env = Environment(num_locations=10, num_passengers=5)
+print(env)
 
