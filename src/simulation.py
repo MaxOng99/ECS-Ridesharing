c4d998374843d3faca28282bb093043ff9db24c8
MaxOng99 <ongyicheng99@gmail.com>
2021-07-12 17:27:27 +0100

    Add simple simulation script

--- src/simulation.py
+++ src/simulation.py
@@ -1,4 +1,10 @@
 from models.environment import Environment
+from algorithms.optimiser import Optimiser
 
 env = Environment(num_locations=10, num_passengers=5, max_coordinates=(100, 100))
-print(env)
+optimiser = Optimiser(env.graph, env.passengers)
+optimal_solution = optimiser.optimise(solution_constructor="nearest_neighbour")
+
+print("Passenger Utilities", "\n")
+for index, passenger in enumerate(env.passengers):
+    print(f"Passenger {index}:", passenger.get_utility(optimal_solution))
