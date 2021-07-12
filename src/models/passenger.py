3cb47ea22109fdd5470e8ca001a34418c18642c8
MaxOng99 <ongyicheng99@gmail.com>
2021-07-12 17:26:12 +0100

    Extend Passenger class
    Add beta parameters, most preffered departure time and arrival time, soluton ranking function and utility function.

--- src/models/passenger.py
+++ src/models/passenger.py
@@ -1,3 +1,5 @@
+import random
+
 class Passenger:
 
     def __init__(self, id, start, destination):
@@ -9,7 +11,27 @@ class Passenger:
         self.id = id
         self.start_id = start
         self.destination_id = destination
+        self.beta = random.uniform(0, 1)
+        self.optimal_departure = 0
+        self.optimal_arrival = 0
+
+    def rank_solutions(self, solutions):
+        return sorted(solutions, key=lambda sol: self.get_utility(sol), reverse=True)
     
+    def get_utility(self, solution):
+        eligible_node_pairs = solution.eligible_node_pairs(self.start_id, self.destination_id)
+
+        max_utility = 0
+        for source_node, target_node in eligible_node_pairs:
+            utility = self.__utility_function(source_node.timestamp, target_node.timestamp)
+            if utility > max_utility:
+                max_utility = utility
+        return max_utility
+        
+    def __utility_function(self, departure_time, arrival_time):
+        return self.beta**(abs(self.optimal_departure - departure_time)) + \
+            self.beta**(abs(self.optimal_arrival - arrival_time))
+
     def __str__(self):
         return f"Passenger {self.id}: {{Start: {self.start_id} | End: {self.destination_id}}}"
     
