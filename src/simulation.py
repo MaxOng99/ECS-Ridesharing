73c23ac0974b0033186a7c8c58e40d7bc1503895
MaxOng99 <ongyicheng99@gmail.com>
2021-07-13 13:55:37 +0100

    Print out solution output

--- src/simulation.py
+++ src/simulation.py
@@ -5,6 +5,8 @@ env = Environment(num_locations=10, num_passengers=5, max_coordinates=(100, 100)
 optimiser = Optimiser(env.graph, env.passengers)
 optimal_solution = optimiser.optimise(solution_constructor="nearest_neighbour")
 
-print("Passenger Utilities", "\n")
+print("Solution:", "\n")
+print(optimal_solution, "\n")
+print("Passenger Utilities: ", "\n")
 for index, passenger in enumerate(env.passengers):
     print(f"Passenger {index}:", passenger.get_utility(optimal_solution))
