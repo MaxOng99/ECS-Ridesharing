8e7ad8678ffd6624b27178c8723cd5f753b651a0
MaxOng99 <ongyicheng99@gmail.com>
2021-08-03 04:19:06 +0100

    Update simulation script
    Test for Iterative Voting algorithm

--- src/simulation.py
+++ src/simulation.py
@@ -1,12 +1,16 @@
 from models.environment import Environment
 from algorithms.optimiser import Optimiser
 
-env = Environment(num_locations=10, num_passengers=5, max_coordinates=(100, 100))
+env = Environment(num_locations=15, num_passengers=20, max_coordinates=(800, 800))
 optimiser = Optimiser(env.graph, env.passengers)
-optimal_solution = optimiser.optimise(solution_constructor="nearest_neighbour")
+
+options = {"algorithm": "iterative_voting", 
+            "parameters": {"voting_rule": "borda_count"}
+            }
+optimal_solution = optimiser.optimise(options)
 
 print("Solution:", "\n")
 print(optimal_solution, "\n")
 print("Passenger Utilities: ", "\n")
 for index, passenger in enumerate(env.passengers):
-    print(f"Passenger {index}:", passenger.get_utility(optimal_solution))
+    print(f"Passenger {index}:", f"Start ({passenger.start_id})", f"End ({passenger.destination_id})", passenger.get_solution_utility(optimal_solution))
