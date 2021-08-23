a9d5cbedf4d7ec29b931f3003be404d6268ad64b
MaxOng99 <ongyicheng99@gmail.com>
2021-08-23 19:01:28 +0100

    Update solution.py and greedy_insert.py
    Linked List management functions such as insertion, removal and checking whether an insertion is valid, are moved to the Solution class.

--- src/algorithms/greedy_insert.py
+++ src/algorithms/greedy_insert.py
@@ -1,5 +1,7 @@
+from numpy import deprecate_with_doc, insert, positive
+from numpy.lib import Arrayterator
 from utils.info_utils import strategy_info
-from pyllist.dllist import dllist, dllistnode
+from pyllist.dllist import dllistnode
 from models.solution import Solution, TourNodeValue
 from models.passenger import Passenger
 from typing import List
@@ -40,32 +42,31 @@ class GreedyInsert:
             
             # Create new Solution
             solution = self.__initialise_new_solution(start_rider)
-
             # Assign n other riders
             other_riders = set(self.passengers) - set([start_rider])
 
             for rider in other_riders:
                 self.__best_allocation(rider, solution)
 
-            solution.update_rider_schedule()
+            solution.create_rider_schedule()
             solutions.append(solution)
 
         # Vote on these solutions
         return solutions[0]
     
     def __initialise_new_solution(self, start_rider):
-        solution = Solution(self.passengers)
+        solution = Solution(self.passengers, self.time_matrix)
 
         # Create TourNodeValue for departure
         depart_node_value = TourNodeValue(start_rider.start_id, 0, start_rider.optimal_departure)
-        depart_node_value.pick_up.add(start_rider)
+        depart_node_value.add_rider(start_rider, 'waiting')
         
         # Create TourNodeValue for arrival
         depart_to_arrival_travel_time = self.time_matrix[(start_rider.start_id, start_rider.destination_id)]
         arrival_time = start_rider.optimal_departure + depart_to_arrival_travel_time
         waiting_time = start_rider.optimal_arrival - arrival_time
         arrive_node_value = TourNodeValue(start_rider.destination_id, arrival_time, waiting_time)
-        arrive_node_value.drop_off.add(start_rider)
+        arrive_node_value.add_rider(start_rider, 'onboard')
 
         solution.llist.append(dllistnode(depart_node_value))
         solution.llist.append(dllistnode(arrive_node_value))
@@ -82,14 +83,14 @@ class GreedyInsert:
 
             # Departure strategy can be allocating this rider to current node,
             # or creating a new node, and insert it before the current node
-            departure_strategy = self.__create_departure_strategy(rider, node, insert_position='before')
+            departure_strategy = self.__create_departure_strategy(rider, node, solution, insert_position='before')
             if departure_strategy:
                 departure_strategies.append(departure_strategy)
             
             # Special case at tail of linked list: Attempt to create an 
             # additional departure strategy by inserting after the current node
             if node.next == None:
-                departure_strategy = self.__create_departure_strategy(rider, node, insert_position='after')
+                departure_strategy = self.__create_departure_strategy(rider, node, solution, insert_position='after')
                 if departure_strategy:
                     departure_strategies.append(departure_strategy)
         
@@ -97,7 +98,7 @@ class GreedyInsert:
         for departure_strategy in departure_strategies:
 
             # Apply departure strategy to linked list
-            departure_strategy.apply_strategy(solution.llist)
+            departure_strategy.apply_strategy(solution)
             depart_node = departure_strategy.allocated_node
 
             # Only consider nodes after depart_node
@@ -105,11 +106,11 @@ class GreedyInsert:
 
                 # Arrival Strategy can be allocating this rider to current node
                 # or create a new node, and insert it after current node.
-                arrival_strategy = self.__create_arrival_strategy(rider, node, depart_node, insert_position='after')
+                arrival_strategy = self.__create_arrival_strategy(rider, node, depart_node, solution, insert_position='after')
                 if arrival_strategy:
 
                     # Apply arrival strategy to llist
-                    arrival_strategy.apply_strategy(solution.llist)
+                    arrival_strategy.apply_strategy(solution)
 
                     # Record utility for this strategy pair
                     departure_time = departure_strategy.allocated_node.value.departure_time
@@ -118,120 +119,62 @@ class GreedyInsert:
                         rider.get_utility_by_time(departure_time, arrival_time)
 
                     # Revert to previous llist state (before applying arrival strategy)
-                    arrival_strategy.revert_strategy(solution.llist)
+                    arrival_strategy.revert_strategy(solution)
             
             # Revert to previous llist state (before applying departure strategy)
-            departure_strategy.revert_strategy(solution.llist)
+            departure_strategy.revert_strategy(solution)
 
         best_depart_strat, best_arrive_strat = max(strategy_util_pairs, key=strategy_util_pairs.get)
-        best_depart_strat.apply_strategy(solution.llist)
+        best_depart_strat.apply_strategy(solution)
         arrival_strat = ArrivalStrategy(best_arrive_strat.strat, best_depart_strat.allocated_node)
-        arrival_strat.apply_strategy(solution.llist)
+        arrival_strat.apply_strategy(solution)
 
-    def __create_departure_strategy(self, rider, ref_node, insert_position='before'):
-        if ref_node.value.location_id == rider.start_id:
-            strategy_details = self.__stay_at_node(rider, ref_node, status='waiting')
+    def __create_departure_strategy(self, rider, ref_node, solution: Solution, insert_position='before'):
+        try:
+            solution.check_valid_insert(ref_node=ref_node, location_id=rider.start_id, position=insert_position)
+            strategy_details = strategy_details = self.__insert_at_node(rider, ref_node, 'waiting', position=insert_position)
             return DepartStrategy(strategy_details)
+        except:
+            if ref_node.value.location_id == rider.start_id:
+                strategy_details = self.__stay_at_node(rider, ref_node, 'waiting')
+                return DepartStrategy(strategy_details)
+            else:
+                return None
 
-        elif self.__valid_insert(ref_node, rider.start_id, position=insert_position):
-            strategy_details = self.__insert_at_node(rider, ref_node, position=insert_position, status='waiting')
-            return DepartStrategy(strategy_details)
-        
-        else:
-            return None
-
-    def __create_arrival_strategy(self, rider, ref_node, depart_node, insert_position='after'):
-        if ref_node.value.location_id == rider.destination_id:
-            strategy_details = self.__stay_at_node(rider, ref_node, status='onboard')
+    def __create_arrival_strategy(self, rider, ref_node, depart_node, solution, insert_position='after'):
+        try:
+            solution.check_valid_insert(ref_node, rider.destination_id, position=insert_position)
+            strategy_details = self.__insert_at_node(rider, ref_node, 'onboard', position=insert_position)
             return ArrivalStrategy(strategy_details, depart_node)
-        
-        elif self.__valid_insert(ref_node, rider.destination_id, position=insert_position):
-            strategy_details = self.__insert_at_node(rider, ref_node, position=insert_position, status='onboard')
-            return ArrivalStrategy(strategy_details, depart_node)
-        else:
-            return None
-
-    def __stay_at_node(self, rider, ref_node, status='waiting'):
-        new_tour_node_value = self.__duplicate_value(ref_node.value)
-
-        if status == 'waiting':
-            new_tour_node_value.pick_up.add(rider)
-        else:
-            new_tour_node_value.drop_off.add(rider)
-        
+        except:
+            if ref_node.value.location_id == rider.destination_id:
+                strategy_details = self.__stay_at_node(rider, ref_node, 'onboard')
+                return ArrivalStrategy(strategy_details, depart_node)
+            else:
+                return None
+                
+    def __stay_at_node(self, rider, ref_node, current_status):
         strategy_details = {
             'action': 'stay',
-            'ref_node': ref_node,
-            'ref_node_old_value': ref_node.value,
-            'ref_node_new_value': new_tour_node_value
+            'rider': rider,
+            'current_status': current_status,
+            'ref_node': ref_node
         }
-        
         return strategy_details
 
-    def __duplicate_value(self, tour_node_value):
-        location_id = tour_node_value.location_id
-        arrival_time = tour_node_value.arrival_time
-        wait_time = tour_node_value.waiting_time
-        duplicated_val = TourNodeValue(location_id, arrival_time, wait_time)
-
-        duplicated_val.pick_up = duplicated_val.pick_up.union(tour_node_value.pick_up)
-        duplicated_val.drop_off = duplicated_val.drop_off.union(tour_node_value.drop_off)
-        return duplicated_val
-
-    def __insert_at_node(self, rider, ref_node, position='before', status='waiting'):
-
-        next_node = ref_node if position == 'before' else ref_node.next
-        new_node_value = self.__create_new_value_by_insertion(rider, ref_node, position, status)
-        next_node_new_value = self.__update_affected_node(new_node_value, next_node)
-
+    def __insert_at_node(self, rider, ref_node, current_status, position='before'):
+        new_node_value = self.__create_new_value_by_insertion(rider, ref_node, position, current_status)
         strategy_details = {
             'action': f'insert_{position}',
             'ref_node': ref_node,
-            'ref_node_old_value': ref_node.value,
-            'new_node_value': new_node_value,
-            'next_node': next_node,
-            'next_node_old_value': None if not next_node else next_node.value,
-            'next_node_new_value': None if not next_node else next_node_new_value
+            'current_status': current_status,
+            'new_node_value': new_node_value
         }
-
         return strategy_details
 
     def __create_new_value_by_insertion(self, rider, ref_node, insert_position, status):
-
-        if status == 'waiting':
-            time_features = self.__calculate_time_features(ref_node, rider.start_id, rider.optimal_departure, insert_position)
-            new_node_value = TourNodeValue(rider.start_id, time_features['arrival'], time_features['wait'])
-            new_node_value.pick_up.add(rider)
-
-        elif status == 'onboard':
-            time_features = self.__calculate_time_features(ref_node, rider.destination_id, rider.optimal_arrival, insert_position)
-            new_node_value = TourNodeValue(rider.destination_id, time_features['arrival'], time_features['wait'])
-            new_node_value.drop_off.add(rider)
-        
-        return new_node_value
-
-    def __update_affected_node(self, new_node_value, next_node):
-        """Updates the immediate node to the right of a newly inserted node"""
-        if next_node:
-
-            # Calculate new arrival and waiting time of next_node
-            # due to inserting new_node_value before next_node
-            new_arrival_time = new_node_value.departure_time + self.time_matrix[(new_node_value.location_id, next_node.value.location_id)]
-            new_waiting_time = next_node.value.departure_time - new_arrival_time
-
-            updated_next_node = self.__duplicate_value(next_node.value)
-            updated_next_node.waiting_time = new_waiting_time
-            updated_next_node.arrival_time = new_arrival_time
-            updated_next_node.departure_time = new_waiting_time + new_arrival_time
-
-            return updated_next_node
-        
-        return None
-
-
-
-    def __calculate_time_features(self, ref_node, new_node_location_id, preferred_time, insert_position):
-        """Calculates the arrival and waiting time for the new node to be inserted"""
+        new_node_location_id = rider.start_id if status == 'waiting' else rider.destination_id
+        preferred_time = rider.optimal_departure if status == 'waiting' else rider.optimal_arrival
 
         # Verify adjacent nodes, before calculating time features for the new TourNodeValue
         if insert_position == 'before':
@@ -248,18 +191,17 @@ class GreedyInsert:
             # Waiting time for new TourNodeValue is dependent on the remainding waiting time
             # for right_node after performing the insert before right_node.
             allowable_waiting_time = right_node.value.departure_time - right_node_new_arrival_time
-            wait_time = min(allowable_waiting_time, preferred_time)
-
+            new_node_wait_time = min(allowable_waiting_time, preferred_time)
+            
             # arrival time is 0, since insertion happens before the head node
-            return {'arrival': 0, 'wait': wait_time}
+            new_node_arrival_time = 0
         
         # Inserting after the tail node. Here, left_node is the tail node
         elif not right_node:
 
             # Waiting time is 
             new_node_arrival_time = left_node.value.departure_time + self.time_matrix[(left_node.value.location_id, new_node_location_id)]
-            wait_time = max(preferred_time - new_node_arrival_time, 0)
-            return {'arrival': new_node_arrival_time, 'wait': wait_time}
+            new_node_wait_time = max(preferred_time - new_node_arrival_time, 0)
         
         else:
             prev_to_new_travel_time = self.time_matrix[(left_node.value.location_id, new_node_location_id)]
@@ -269,52 +211,12 @@ class GreedyInsert:
             next_node_arrival_time = new_node_arrival_time + new_to_next_travel_time
 
             allowable_delay = right_node.value.departure_time - next_node_arrival_time
-            wait_time = min(allowable_delay, max(preferred_time - new_node_arrival_time, 0))
-            return {'arrival': new_node_arrival_time, 'wait': wait_time}
+            new_node_wait_time = min(allowable_delay, max(preferred_time - new_node_arrival_time, 0))
 
-    def __valid_insert(self, ref_node, location_id, position='before'):
+        new_node_value = TourNodeValue(new_node_location_id, new_node_arrival_time, new_node_wait_time)
+        new_node_value.add_rider(rider, status)
         
-        # Verify adjacent nodes, before attempting to insert between them
-        if position == 'before':
-            left_node = ref_node.prev
-            right_node = ref_node
-        else:
-            left_node = ref_node
-            right_node = ref_node.next
-        
-        # If either nodes have the same location_id, reject insert
-        if left_node and left_node.value.location_id == location_id or \
-            right_node and right_node.value.location_id == location_id:
-            return False
-        
-        # Left node is empty: right_node is the head of linked list.
-        # Accept the insertion if inserting before the head node does not cause
-        # the new arrival time of the head node to exceed its departure time
-        elif not left_node:
-            right_node_new_arrival_time = self.time_matrix[(location_id, right_node.value.location_id)]
-            if right_node_new_arrival_time <= right_node.value.departure_time:
-                return True
-            else:
-                return False
-        
-        # Right node is empty (left_node is the tail of linked list) There is no right_node
-        # to check wehther it's new arrival time exceeds its departure time due to the insert.
-        # Therefore, always accept the insertion in this case
-        elif not right_node:
-            return True
-        
-        # Accept insertion if inserting between these two nodes do not cause the new 
-        # arrival time of the right_node to exceed its departure time
-        else:
-            left_to_new_travel_time = self.time_matrix[(left_node.value.location_id, location_id)]
-            new_to_right_travel_time = self.time_matrix[(location_id, right_node.value.location_id)]
-
-            new_node_arrival_time = left_node.value.departure_time + left_to_new_travel_time
-            right_node_new_arrival_time = new_node_arrival_time + new_to_right_travel_time
-            if right_node_new_arrival_time <= right_node.value.departure_time:
-                return True
-            else:
-                return False
+        return new_node_value
 
 # HELPER CLASS
 class Strategy:
@@ -341,7 +243,7 @@ class Strategy:
         self.strat = strategy_dictionary
         self.allocated_node: dllistnode = None
 
-    def apply_strategy(self, llist: dllist):
+    def apply_strategy(self, solution: Solution):
         """Carry out this strategy (wait/insert), affecting the state of llist. If there are more strategies
         to check, ensure that revert_strategy is called afterwards to revert the changes, so that other
         strategies can be correctly applied with an untouched state of the linked list.
@@ -350,20 +252,16 @@ class Strategy:
         strat = self.strat
         # Either wait at the current node, or insert a new one
         if strat['action'] == 'stay':
-            strat['ref_node'].value = strat['ref_node_new_value']
+            strat['ref_node'].value.add_rider(strat['rider'], strat['current_status'])
             self.allocated_node = strat['ref_node']
 
         elif strat['action'] == 'insert_before':
-            self.allocated_node = llist.insert(strat['new_node_value'], before=strat['ref_node'])
-            if strat['next_node']:
-                strat['next_node'].value = strat['next_node_new_value']
+            self.allocated_node = solution.insert_before(ref_node=strat['ref_node'], new_node_value=strat['new_node_value'])
 
         elif strat['action'] == 'insert_after':
-            self.allocated_node = llist.insert(strat['new_node_value'], after=strat['ref_node'])
-            if strat['next_node']:
-                strat['next_node'].value = strat['next_node_new_value']
+            self.allocated_node = solution.insert_after(ref_node=strat['ref_node'], new_node_value=strat['new_node_value'])
 
-    def revert_strategy(self, llist: dllist):
+    def revert_strategy(self, solution: Solution):
         """This function should only be called after apply_strategy() is called. This reverts
         the linked list back to its original state before this strategy is applied in order
         to ensure that the changes made by this strategy does not affect other strategies being tested.
@@ -371,15 +269,13 @@ class Strategy:
         strat = self.strat
 
         if strat['action'] == 'stay':
-            strat['ref_node'].value = strat['ref_node_old_value']
-        
+            strat['ref_node'].value.remove_rider(strat['rider'], strat['current_status'])
+
         elif strat['action'] == 'insert_before' or \
             strat['action'] == 'insert_after':
-            llist.remove(self.allocated_node)
 
-            if strat['next_node']:
-                strat['next_node'].value = strat['next_node_old_value']
-    
+            solution.remove_node(self.allocated_node)
+
     def __repr__(self) -> str:
         return self.__str__()
 
--- src/models/solution.py
+++ src/models/solution.py
@@ -13,6 +13,20 @@ class TourNodeValue:
         self.pick_up = set()
         self.drop_off = set()
 
+    def add_rider(self, rider, current_status):
+        if current_status == 'waiting':
+            self.pick_up.add(rider)
+        
+        elif current_status == 'onboard':
+            self.drop_off.add(rider)
+    
+    def remove_rider(self, rider, current_status):
+        if current_status == 'waiting':
+            self.pick_up.remove(rider)
+        
+        elif current_status == 'onboard':
+            self.drop_off.remove(rider)
+
     def __repr__(self) -> str:
         return self.__str__()
 
@@ -20,12 +34,74 @@ class TourNodeValue:
         return f"({self.location_id}, {self.arrival_time}, {self.waiting_time}, P:{self.pick_up}, D:{self.drop_off})"
 
 class Solution:
-    def __init__(self, riders: Set['Passenger']):
+    def __init__(self, riders: Set['Passenger'], time_matrix):
         self.llist = dllist()
         self.rider_schedule = {"departure": dict(), "arrival": dict()}
         self.riders = sorted(list(riders), key=lambda x: x.id)
+        self.time_matrix = time_matrix
+
+    def insert_after(self, ref_node, new_node_value):
+        self.check_valid_insert(ref_node, new_node_value.location_id, position='after')
+        affected_node = ref_node.next
+        new_node = self.llist.insert(new_node_value, after=ref_node)
+        self.__updated_affected_node(affected_node)
+        return new_node
     
-    def update_rider_schedule(self) -> Dict[str, Dict[int, int]]:
+    def insert_before(self, ref_node, new_node_value):
+        self.check_valid_insert(ref_node, new_node_value.location_id, position='before')
+        affected_node = ref_node
+        new_node = self.llist.insert(new_node_value, before=ref_node)
+        self.__updated_affected_node(affected_node)
+        return new_node
+
+    def check_valid_insert(self, ref_node, location_id, position='before'):
+        
+        # Verify adjacent nodes, before attempting to insert between them
+        if position == 'before':
+            left_node = ref_node.prev
+            right_node = ref_node
+        else:
+            left_node = ref_node
+            right_node = ref_node.next
+        
+        # If either nodes have the same location_id, reject insert
+        if left_node and left_node.value.location_id == location_id or \
+            right_node and right_node.value.location_id == location_id:
+            raise ValueError("Adjacent node have the same location_id")
+        
+        # Left node is empty: right_node is the head of linked list.
+        # Accept the insertion if inserting before the head node does not cause
+        # the new arrival time of the head node to exceed its departure time
+        elif not left_node:
+            right_node_new_arrival_time = self.time_matrix[(location_id, right_node.value.location_id)]
+            if right_node_new_arrival_time > right_node.value.departure_time:
+                raise ValueError("New arrival time of next node exceeds its departure time")
+
+        # Right node is empty (left_node is the tail of linked list) There is no right_node
+        # to check wehther it's new arrival time exceeds its departure time due to the insert.
+        # Therefore, always accept the insertion in this case
+        elif not right_node:
+            return None
+
+        # Accept insertion if inserting between these two nodes do not cause the new 
+        # arrival time of the right_node to exceed its departure time
+        else:
+            left_to_new_travel_time = self.time_matrix[(left_node.value.location_id, location_id)]
+            new_to_right_travel_time = self.time_matrix[(location_id, right_node.value.location_id)]
+
+            new_node_arrival_time = left_node.value.departure_time + left_to_new_travel_time
+            right_node_new_arrival_time = new_node_arrival_time + new_to_right_travel_time
+            if right_node_new_arrival_time > right_node.value.departure_time:
+                raise ValueError("New arrival time of next node exceeds its departure time")
+        
+        return None
+
+    def remove_node(self, ref_node):
+        affected_node = ref_node.next
+        self.llist.remove(ref_node)
+        self.__updated_affected_node(affected_node)
+    
+    def create_rider_schedule(self) -> Dict[str, Dict[int, int]]:
         for node in self.llist.iternodes():
             departure_time = node.value.departure_time
             arrival_time = node.value.arrival_time
@@ -37,6 +113,25 @@ class Solution:
                 self.rider_schedule['arrival'][rider.id] = arrival_time
         
         return self.rider_schedule
+
+    def __updated_affected_node(self, affected_node):
+        if affected_node:
+            if affected_node.prev:
+                affecting_node = affected_node.prev
+                source = affecting_node.value.location_id
+                target = affected_node.value.location_id
+
+                new_arrival_time = affecting_node.value.departure_time + self.time_matrix[(source, target)]
+                new_waiting_time = affected_node.value.departure_time - new_arrival_time
+
+                affected_node.value.arrival_time = new_arrival_time
+                affected_node.value.waiting_time = new_waiting_time            
+            else:
+                affected_node.value.arrival_time = 0
+                affected_node.value.waiting_time = affected_node.value.departure_time
+        
+            if affected_node.value.waiting_time < 0:
+                print("bruh dude")
 
     def __str__(self) -> str:
         return solution_info(self)
