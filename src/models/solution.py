73e9b9f080b029c97d16e320ef3b5c5724ea7bb7
MaxOng99 <ongyicheng99@gmail.com>
2021-08-27 20:05:45 +0100

    Generalise impact of insertion
    __update() function now handles cascading updates when a new node is inserted, instead of a single update of the next node

--- src/models/solution.py
+++ src/models/solution.py
@@ -1,10 +1,11 @@
 from typing import Dict, Set
-from pyllist import dllist
+from pyllist import dllist, dllistnode
 from utils.info_utils import solution_info
 
 class TourNodeValue:
 
     def __init__(self, location_id: int, arrival_time: int, waiting_time: int) -> None:
+
         self.location_id = location_id
         self.arrival_time = arrival_time
         self.waiting_time = waiting_time
@@ -38,82 +39,49 @@ class TourNodeValue:
         return f"({self.location_id}, {self.arrival_time}, {self.waiting_time}, P:{self.pick_up}, D:{self.drop_off})"
 
 class Solution:
+
     def __init__(self, agents: Set["Agent"], time_matrix):
         self.llist = dllist()
         self.rider_schedule = {"departure": dict(), "arrival": dict()} # nullify
         self.agents = sorted(list(agents), key=lambda x: x.rider.id)
         self.time_matrix = time_matrix
 
-    def insert_after(self, ref_node, new_node_value):
-        self.check_valid_insert(ref_node, new_node_value.location_id, position='after')
-        affected_node = ref_node.next
-        new_node = self.llist.insert(new_node_value, after=ref_node)
-        self.__updated_affected_node(affected_node)
-        return new_node
-    
-    def insert_before(self, ref_node, new_node_value):
-        self.check_valid_insert(ref_node, new_node_value.location_id, position='before')
-        affected_node = ref_node
-        new_node = self.llist.insert(new_node_value, before=ref_node)
-        self.__updated_affected_node(affected_node)
-        return new_node
-    
-    def append(self, new_node_value):
-        new_node = self.llist.append(new_node_value)
-        return new_node
-        
     def head(self):
         return self.llist.first
     
     def tail(self):
         return self.llist.last
 
-    def check_valid_insert(self, ref_node, location_id, position='before'):
-        
-        # Verify adjacent nodes, before attempting to insert between them
-        if position == 'before':
-            left_node = ref_node.prev
-            right_node = ref_node
+    def iterator(self, start_node: dllistnode=None):
+        if start_node:
+            return start_node.iternext()
         else:
-            left_node = ref_node
-            right_node = ref_node.next
-        
-        # If either nodes have the same location_id, reject insert
-        if left_node and left_node.value.location_id == location_id or \
-            right_node and right_node.value.location_id == location_id:
-            raise ValueError("Adjacent node have the same location_id")
-        
-        # Left node is empty: right_node is the head of linked list.
-        # Accept the insertion if inserting before the head node does not cause
-        # the new arrival time of the head node to exceed its departure time
-        elif not left_node:
-            right_node_new_arrival_time = self.time_matrix[(location_id, right_node.value.location_id)]
-            if right_node_new_arrival_time > right_node.value.departure_time:
-                raise ValueError("New arrival time of next node exceeds its departure time")
+            return self.llist.iternodes()
 
-        # Right node is empty (left_node is the tail of linked list) There is no right_node
-        # to check wehther it's new arrival time exceeds its departure time due to the insert.
-        # Therefore, always accept the insertion in this case
-        elif not right_node:
-            return None
+    def insert_after(self, ref_node, new_node: dllistnode):
+        if not self.__valid_insert(new_node.value, ref_node, position='after'):
+            raise Exception("Invalid Insert")
 
-        # Accept insertion if inserting between these two nodes do not cause the new 
-        # arrival time of the right_node to exceed its departure time
-        else:
-            left_to_new_travel_time = self.time_matrix[(left_node.value.location_id, location_id)]
-            new_to_right_travel_time = self.time_matrix[(location_id, right_node.value.location_id)]
-
-            new_node_arrival_time = left_node.value.departure_time + left_to_new_travel_time
-            right_node_new_arrival_time = new_node_arrival_time + new_to_right_travel_time
-            if right_node_new_arrival_time > right_node.value.departure_time:
-                raise ValueError("New arrival time of next node exceeds its departure time")
-        
-        return None
-
-    def remove_node(self, ref_node):
         affected_node = ref_node.next
-        self.llist.remove(ref_node)
-        self.__updated_affected_node(affected_node)
+        new_node = self.llist.insert(new_node, after=ref_node)
+        self.__update_after_insert(affected_node)
+        return new_node
+
+    def insert_before(self, ref_node, new_node: dllistnode):
+        if not self.__valid_insert(new_node.value, ref_node, position='before'):
+            raise Exception("Invalid Insert")
+
+        affected_node = ref_node
+        new_node = self.llist.insert(new_node, before=ref_node)
+        self.__update_after_insert(affected_node)
+        return new_node
+    
+    def append(self, new_node: dllistnode):
+        if not self.__valid_insert(new_node.value, self.tail(), position='after'):
+            raise Exception("Invalid Insert")
+
+        new_node = self.llist.append(new_node)
+        return new_node
     
     def create_rider_schedule(self) -> Dict[str, Dict[int, int]]:
         for node in self.llist.iternodes():
@@ -128,22 +96,52 @@ class Solution:
         
         return self.rider_schedule
 
-    def __updated_affected_node(self, affected_node):
-        if affected_node:
-            if affected_node.prev:
-                affecting_node = affected_node.prev
-                source = affecting_node.value.location_id
-                target = affected_node.value.location_id
+    def __valid_insert(self, new_node_value, ref_node, position=None):
 
-                new_arrival_time = affecting_node.value.departure_time + self.time_matrix[(source, target)]
-                new_waiting_time = affected_node.value.departure_time - new_arrival_time
-
-                affected_node.value.arrival_time = new_arrival_time
-                affected_node.value.waiting_time = new_waiting_time            
-            else:
-                affected_node.value.arrival_time = 0
-                affected_node.value.waiting_time = affected_node.value.departure_time
+        if not self.tail():
+            return True 
         
+        if ref_node:
+            new_location = new_node_value.location_id
+            arrival_time = new_node_value.arrival_time
+
+            if position == 'before':
+                left_node = ref_node.prev
+                right_node = ref_node
+            elif position == 'after':
+                left_node = ref_node
+                right_node = ref_node.next
+
+            # If either nodes have the same location_id, reject insert
+            if left_node and left_node.value.location_id == new_location or \
+                right_node and right_node.value.location_id == new_location:
+                return False
+            
+            elif left_node:
+                left_node_location = left_node.value.location_id
+                left_node_depart_time = left_node.value.departure_time
+                travel_time = self.time_matrix[(left_node_location, new_location)]
+                if left_node_depart_time + travel_time != arrival_time:
+                    return False
+        return True
+
+    def __update_after_insert(self, affected_node):
+        if affected_node:
+            for node in affected_node.iternext():
+                prev = node.prev
+                travel_time = self.time_matrix[(prev.value.location_id, node.value.location_id)]
+                arrival_time = prev.value.departure_time + travel_time
+
+                if arrival_time > affected_node.value.departure_time:
+                    affected_node.value.departure_time = arrival_time
+                    affected_node.value.arrival_time = arrival_time
+                    affected_node.value.waiting_time = 0
+                else:
+                    new_waiting_time = affected_node.value.departure_time - arrival_time
+                    affected_node.value.arrival_time = arrival_time
+                    affected_node.value.waiting_time = new_waiting_time
+                    break
+                
     def __str__(self) -> str:
         return solution_info(self)
 
