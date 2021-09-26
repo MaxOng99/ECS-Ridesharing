fe93a246390f2b24ecd06662f6e325034b30d36e
MaxOng99 <ongyicheng99@gmail.com>
2021-09-27 05:22:45 +0800

    Add distribution plotting functions

--- src/utils/graph_utils.py
+++ src/utils/graph_utils.py
@@ -1,37 +1,73 @@
 import igraph as ig
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from models.graph import Graph
 
-def plot_graph(igraph: ig.Graph) -> None:
-    visual_style = {
-        "bbox": (600, 600),
-        "margin": 30,
-        "vertex_label_size": 10,
-        "edge_label_size": 10,
-        "vertex_size": 40
-    }
-    layout = [(x, -y) for x, y in igraph.vs["coordinate"]]
-    for v in igraph.vs:
-        if v['is_centroid']:
-            v['color'] = 'blue'
+def plot_graph(graph: Graph, path: str) -> None:
     
-    for e in igraph.es:
-        source = igraph.vs[e.source]
-        target = igraph.vs[e.target]
+    data = dict()
+    igraph = graph.igraph
+    centroids = list(igraph.vs.select(is_centroid_eq=True))
 
-        if not source['is_centroid'] or \
-            not target['is_centroid']:
-            e['width'] = 0
+    for centroid_id, centroid in enumerate(centroids):
+        x, y = centroid['coordinate']
+        locations = centroid['cluster']
 
-    centroids = igraph.vs.select(is_centroid_eq=True)
-    for centroid in centroids:
-        cluster = centroid['cluster']
-        for location in cluster:
-            edge_id = igraph.get_eid(centroid.index, location.index)
-            edge = igraph.es[edge_id]
-            edge['width'] = 1
+        data[centroid['location_id']] = {
+            "x": x,
+            "y": y,
+            "cluster": centroid_id,
+            "is_cluster": True
+        }
 
-    
-    igraph.vs['label'] = [(int(x), int(y)) for x, y in igraph.vs['coordinate']]
-    ig.plot(igraph, layout=layout, **visual_style)
+        for location in locations:
+            x, y = location['coordinate']
+            data[location['location_id']] = {
+                "x": x,
+                "y": y,
+                "cluster": centroid_id,
+                "is_cluster": False
+            }
+
+    sns.set_style("whitegrid")
+    fig, ax = plt.subplots()
+    df = pd.DataFrame.from_dict(data, orient='index')
+
+    for _, point in df.iterrows():
+        if point['is_cluster']:
+            ax.text(point['x'], point['y'], point['cluster'])
+    colors = sns.color_palette("deep", len(centroids))
+
+    sns.scatterplot(data=df , x="x", y="y", hue="cluster", palette=colors, ax=ax)
+    fig.savefig(path)
+
+def plot_beta_distribution(distribution, path):
+    sns.displot(distribution, kind="hist")
+    plt.xlabel("beta")
+    plt.subplots_adjust(bottom=0.1)
+    plt.savefig(path)
+
+def plot_preference_distribution(dist, path):
+
+    # Convert unit time to hours
+    depart_times = []
+    arrive_times = []
+
+    for x, y in dist:
+        depart_times.append(x/60)
+        arrive_times.append(y/60)
+
+
+    df = pd.concat(axis=0, ignore_index=True, objs=[
+        pd.DataFrame.from_dict({'hours': depart_times, 'journey_type': 'depart_times'}),
+        pd.DataFrame.from_dict({'hours': arrive_times, 'journey_type': 'arrive_times'})
+    ])
+    fig, ax = plt.subplots()
+    sns.histplot(data=df, x='hours', hue='journey_type', multiple='dodge', ax=ax)
+    plt.savefig(path)
+
+
 
 
 
