533f46cf94fd1a3c020216fcb600419abb6e934b
MaxOng99 <ongyicheng99@gmail.com>
2021-10-06 21:39:24 +0800

    remove weighted based voting

--- src/algorithms/voting_rules.py
+++ src/algorithms/voting_rules.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class VotingRules:
 
-    def borda_count(candidates: Set[object], ranking_functions: List[Callable], weights: List[float]):
+    def borda_count(candidates: Set[object], ranking_functions: List[Callable]):
         """Borda count implementation
 
         Args:
@@ -16,28 +16,28 @@ class VotingRules:
         """
         num_of_candidates = len(candidates)
         scores = {candidate: 0 for candidate in candidates}
-
-        for ranking_function, weight in zip(ranking_functions, weights):
+        for ranking_function in ranking_functions:
             ranked_candidates = ranking_function(candidates)
             for rank_index, candidate in enumerate(ranked_candidates):
-                scores[candidate] += weight*((num_of_candidates - 1) - rank_index)
+                scores[candidate] += (num_of_candidates - 1) - rank_index
         
         # Use np.random to account for the fact that there could be ties
         best_candidate = max(scores, key=scores.get)
         best_score = scores[best_candidate]
         indifferent_candidates = \
             [candidate for candidate in candidates if scores[candidate] == best_score]
-
+        
         return np.random.choice(indifferent_candidates)
 
-    def popularity(candidates: Set[object], ranking_functions: List[Callable], weights: List[float]):
+    def popularity(candidates: Set[object], ranking_functions: List[Callable]):
 
         scores = {candidate: 0 for candidate in candidates}
     
-        for ranking_function, weight in zip(ranking_functions, weights):
+        for ranking_function in ranking_functions:
             ranked_candidates = ranking_function(candidates)
-            first_choice = ranked_candidates[0]
-            scores[first_choice] += 1*weight
+            if len(ranked_candidates) > 0:
+                first_choice = ranked_candidates[0]
+                scores[first_choice] += 1
         # Use np.random to account for ties
         best_candidate = max(scores, key=scores.get)
         best_score =scores[best_candidate]
